3aa3211fb489f31bb3a507a9ca37f8b4a3a4a148
Georgios Koutsoumpakis <gkoutsou90@gmail.com>
2025-06-01 13:02:47 +0200

    wip: refactoring timers to use system-clock

--- src/gameboy/timer.rs
+++ src/gameboy/timer.rs
@@ -3,12 +3,11 @@ use log::{info, trace};
 use super::memory_bus::MemoryAccessor;
 
 pub struct Timer {
-    /// FF04
-    /// This register is incremented at a rate of 16384Hz (~16779Hz on SGB).
-    /// Writing any value to this register resets it to $00. Additionally,
-    /// this register is reset when executing the stop instruction, and only
-    /// begins ticking again once stop mode ends.
-    div: u8,
+    // FF04
+    // This register is incremented at a rate of 16384Hz (~16779Hz on SGB).
+    // Writing any value to this register resets it to $00. Additionally,
+    // this register is reset when executing the stop instruction, and only
+    // begins ticking again once stop mode ends.
     /// FF05
     /// This timer is incremented at the clock frequency specified by the TAC
     /// register ($FF07). When the value overflows (exceeds $FF) it is reset to
@@ -23,9 +22,9 @@ pub struct Timer {
     tac: u8,
 
     // temporary:
-    div_counter: u32,
-    tima_counter: u32,
-    tima_clock: u32,
+    system_clock: u16,
+    tima_counter: u8,
+    tima_overflow_delay: bool,
 }
 
 impl Timer {
@@ -33,15 +32,15 @@ impl Timer {
         (self.tac & (1 << 2)) > 0
     }
 
-    fn clock(&self) -> u32 {
+    fn tima_clock_bit(&self) -> u8 {
         let selected = self.tac & 0x3;
         info!("selected {}", selected);
 
         match selected {
-            0 => 256,
-            1 => 4,
-            2 => 16,
-            3 => 64,
+            0 => 9, // every 256 M-Ticks - or every 4th div
+            1 => 3, // every 4 M-Ticks
+            2 => 5, // every 16 M-Ticks
+            3 => 7, // every 64 M-Ticks - or every div
             _clock => panic!("unknown tima clock: {}", _clock),
         }
     }
@@ -51,43 +50,60 @@ impl Timer {
         // div step:   16384 Hz
         // so a div is stepped every 255.981445313 dots
         // or 256/4 = 64 m_ticks
-        let m_ticks = dots / 4;
-        self.div_counter += m_ticks;
-        if self.div_counter >= 64 {
-            self.div_counter -= 64;
-            self.div = self.div.wrapping_add(1);
-            // debug!("div: {}", self.div)
-        }
+        let old_clock = self.system_clock;
+        self.system_clock = self.system_clock.wrapping_add(dots as u16);
+
+        // if self.tima_overflow_delay {
+        //     self.tima_overflow_delay = false;
+        //     return true;
+        // }
 
         if !self.tima_enabled() {
             return false;
         }
 
-        self.tima_counter += m_ticks;
-        let clock = self.tima_clock;
-
-        if self.tima_counter >= clock {
-            self.tima_counter -= clock;
-            self.tima = self.tima.wrapping_add(1);
-
-            if self.tima == 0 {
-                self.tima = self.tma;
-                return true;
-            }
+        if Self::has_bit_gone_low(old_clock, self.system_clock, self.tima_clock_bit()) {
+            println!(
+                "{:#b} {:#b} - {}",
+                old_clock,
+                self.system_clock,
+                self.system_clock >> 8,
+            );
+            // self.tima_overflow_delay = self.timer_tick();
+            // return false;
+            return self.timer_tick();
         }
+
+        // TODO: On monochrome consoles, disabling the timer if the currently selected bit is set,
+        //  will send a “Timer tick” once.
         false
+    }
+
+    fn has_bit_gone_low(old: u16, new: u16, bit: u8) -> bool {
+        let original_bit = (old >> bit) & 1;
+        let new_bit = (new >> bit) & 1;
+        original_bit != new_bit && new_bit == 0 // TODO this has issue when stepping too much, right? for loop instead?
+    }
+
+    fn timer_tick(&mut self) -> bool {
+        self.tima = self.tima.wrapping_add(1);
+
+        if self.tima == 0 {
+            self.tima = self.tma;
+            return true;
+        }
+        return false;
     }
 
     pub fn new() -> Self {
         Timer {
-            div: 0xab,
             tima: 0,
             tma: 0,
             tac: 0xf8,
             // helpers
-            div_counter: 0,
+            system_clock: 0xab << 8,
             tima_counter: 0,
-            tima_clock: 0,
+            tima_overflow_delay: false,
         }
     }
 }
@@ -96,7 +112,7 @@ impl MemoryAccessor for Timer {
     fn get(&self, location: usize) -> u8 {
         trace!("Read Timer: {:#x}", location);
         match location {
-            0xFF04 => self.div,
+            0xFF04 => (self.system_clock >> 8) as u8,
             0xFF05 => self.tima,
             0xFF06 => self.tma,
             0xFF07 => self.tac,
@@ -109,16 +125,12 @@ impl MemoryAccessor for Timer {
         match location {
             0xFF04 => {
                 // writing any value resets it
-                self.div = 0;
-                self.div_counter = 0;
-                self.tima_counter = 0; // tima uses same counter as div. So both get reset
+                self.system_clock = 0;
+                println!("RESET");
             }
             0xFF05 => self.tima = value,
             0xFF06 => self.tma = value,
-            0xFF07 => {
-                self.tac = value;
-                self.tima_clock = self.clock();
-            }
+            0xFF07 => self.tac = value,
             _ => panic!(
                 "timer register location write: {:#x} - {:#x}",
                 location, value
