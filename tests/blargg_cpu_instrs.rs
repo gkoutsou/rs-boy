a8d6ad07cd2bc07c6545541ef2b47988ce9efade
Georgios Koutsoumpakis <gkoutsou90@gmail.com>
2025-06-04 22:24:15 +0200

    add tests for the cpu_instr

---
+++ tests/blargg_cpu_instrs.rs
@@ -0,0 +1,50 @@
+#[cfg(test)]
+mod test {
+    use rs_boy::gameboy::GameBoy;
+    use std::path::Path;
+    use test_case::test_case;
+
+    const ROMPATH: &str = "tests/blargg/cpu_instrs/individual";
+
+    const SERIAL_DATA_LOCATION: usize = 0xff01;
+    const SERIAL_TRANSFER_LOCATION: usize = 0xff02;
+
+    #[test_case("01-special.gb" ; "1 special")]
+    #[test_case("02-interrupts.gb" ; "2 interrupts")]
+    #[test_case("03-op sp,hl.gb" ; "3 op sp,hl")]
+    #[test_case("04-op r,imm.gb" ; "4 op r,imm")]
+    #[test_case("05-op rp.gb" ; "5 op rp")]
+    #[test_case("06-ld r,r.gb" ; "6 ld r,r")]
+    #[test_case("07-jr,jp,call,ret,rst.gb" ; "7 jr,jp,call,ret,rst")]
+    #[test_case("08-misc instrs.gb" ; "8 misc instrs")]
+    #[test_case("09-op r,r.gb" ; "9 op r,r")]
+    #[test_case("10-bit ops.gb" ; "10 bit ops")]
+    #[test_case("11-op a,(hl).gb" ; "11 op a,(hl)")]
+    fn blargg_cpu_instrs(rom: &str) {
+        let mut gb = GameBoy::new(Path::new(ROMPATH).join(rom).to_str().unwrap());
+        let mut ongoing_transfer = false;
+        let mut output = String::new();
+        loop {
+            let status = gb.memory_read(SERIAL_TRANSFER_LOCATION);
+            if (status & (1 << 7)) > 0 && !ongoing_transfer {
+                ongoing_transfer = true;
+                let ascii = gb.memory_read(SERIAL_DATA_LOCATION);
+                output.push(ascii as char);
+                // Hacky, but if a serial transfer is requested, read and directly flag it as done
+                gb.memory_write(SERIAL_TRANSFER_LOCATION, 1);
+            } else if status & 1 << 7 == 0 && ongoing_transfer {
+                ongoing_transfer = false;
+            }
+
+            if output.contains("Passed") {
+                break;
+            } else if output.contains("Failed") {
+                break;
+            }
+
+            gb.step();
+        }
+
+        assert_eq!(output.contains("Passed"), true);
+    }
+}
--- tests/blargg_dmg_sound.rs
+++ tests/blargg_dmg_sound.rs
@@ -1,51 +1,48 @@
-use log::info;
-use rs_boy::gameboy::GameBoy;
-use std::path::Path;
+#[cfg(test)]
+mod test {
+    use rs_boy::gameboy::GameBoy;
+    use std::path::Path;
+    use test_case::test_case;
 
-const ROMPATH: &str = "tests/blargg/dmg_sound/rom_singles";
+    const ROMPATH: &str = "tests/blargg/dmg_sound/rom_singles";
 
-const STATUS_LOCATION: usize = 0xA000;
-const TEST_RUNNING_VALUE: u8 = 0x80;
+    const STATUS_LOCATION: usize = 0xA000;
+    const TEST_RUNNING_VALUE: u8 = 0x80;
 
-macro_rules! test {
-    ($fn_name:ident, $rom:expr) => {
-        #[test]
-        fn $fn_name() {
-            let mut gb = GameBoy::new(Path::new(ROMPATH).join($rom).to_str().unwrap());
+    #[test_case("01-registers.gb" ; "1 registers")]
+    #[test_case("02-len ctr.gb" ; "2 len_ctr")]
+    #[test_case("03-trigger.gb" ; "3 trigger")]
+    #[test_case("04-sweep.gb" ; "4 sweep")]
+    #[test_case("05-sweep details.gb" ; "5 sweep_details")]
+    #[test_case("06-overflow on trigger.gb" ; "6 overflow_on_trigger")]
+    #[test_case("07-len sweep period sync.gb" ; "7 len_sweep_period_sync")]
+    #[test_case("08-len ctr during power.gb" ; "8 leb_ctr_during_power")]
+    #[test_case("09-wave read while on.gb" ; "9 wave_read_while_on")]
+    #[test_case("10-wave trigger while on.gb" ; "10 wave_trigger_while_on")]
+    #[test_case("11-regs after power.gb" ; "11 regs_after_power")]
+    #[test_case("12-wave write while on.gb" ; "12 wave_write_while_on")]
+    fn acceptance(rom: &str) {
+        let mut gb = GameBoy::new(Path::new(ROMPATH).join(rom).to_str().unwrap());
 
-            let mut found = false;
-            loop {
-                let status = gb.memory_read(STATUS_LOCATION);
+        let mut found = false;
+        loop {
+            let status = gb.memory_read(STATUS_LOCATION);
 
-                if !found && status == TEST_RUNNING_VALUE {
-                    found = true;
-                } else if found && status != TEST_RUNNING_VALUE {
-                    break;
-                }
-
-                gb.step();
+            if !found && status == TEST_RUNNING_VALUE {
+                found = true;
+            } else if found && status != TEST_RUNNING_VALUE {
+                break;
             }
 
-            assert_eq!(gb.memory_read(0xA001), 0xDE);
-            assert_eq!(gb.memory_read(0xA002), 0xB0);
-            assert_eq!(gb.memory_read(0xA003), 0x61);
-
-            // Not correct
-            // TODO do I need to read the text after each test?
-            assert_eq!(gb.memory_read(STATUS_LOCATION), 0);
+            gb.step();
         }
-    };
-}
 
-test!(registers, "01-registers.gb");
-test!(len_ctr, "02-len ctr.gb");
-test!(trigger, "03-trigger.gb");
-test!(sweep, "04-sweep.gb");
-test!(sweep_details, "05-sweep details.gb");
-test!(overflow_on_trigger, "06-overflow on trigger.gb");
-test!(len_sweep_period_sync, "07-len sweep period sync.gb");
-test!(leb_ctr_during_power, "08-len ctr during power.gb");
-test!(wave_read_while_on, "09-wave read while on.gb");
-test!(wave_trigger_while_on, "10-wave trigger while on.gb");
-test!(regs_after_power, "11-regs after power.gb");
-test!(wave_write_while_on, "12-wave write while on.gb");
+        assert_eq!(gb.memory_read(0xA001), 0xDE);
+        assert_eq!(gb.memory_read(0xA002), 0xB0);
+        assert_eq!(gb.memory_read(0xA003), 0x61);
+
+        // Not correct
+        // TODO do I need to read the text after each test?
+        assert_eq!(gb.memory_read(STATUS_LOCATION), 0);
+    }
+}
