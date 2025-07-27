1042e9c9ab7eb8f975506b2f5138e8538910b769
Georgios Koutsoumpakis <gkoutsou90@gmail.com>
2025-07-27 10:19:08 +0200

    mealybug tests are all failing miserably

--- lib_rs_boy/tests/mealybug_tearoom_ppu.rs
+++ lib_rs_boy/tests/mealybug_tearoom_ppu.rs
@@ -1,6 +1,6 @@
 #[cfg(test)]
 mod test {
-     use image::{DynamicImage, ImageReader};
+    use image::{DynamicImage, ImageBuffer, ImageReader, Rgb};
     use lib_rs_boy::gameboy::GameBoy;
     use lib_rs_boy::io::files::load_file;
     use std::path::Path;
@@ -39,13 +39,15 @@ mod test {
     #[test_case("m3_wx_5_change" ; "m3_wx_5_change")]
     #[test_case("m3_wx_6_change" ; "m3_wx_6_change")]
     #[test_case("win_without_bg" ; "win_without_bg")]
-    fn ppu(rom: &str) {
-        let rom = load_file(Path::new(TEST_DIR_PATH).join(rom.to_owned()+".gb").as_path()).unwrap();
+    fn ppu(rom_name: &str) {
+        let rom = load_file(Path::new(TEST_DIR_PATH).join(rom_name.to_owned() + ".gb").as_path()).unwrap();
+
+        let png_path = rom_name.to_owned() + "_dmg_blob.png";
+        let img: DynamicImage = ImageReader::open(Path::new(TEST_DIR_PATH).join(png_path).as_path()).unwrap()
+            .decode().unwrap();
+
         let mut gb = GameBoy::new();
         gb.load_rom(rom);
-
-        let img: DynamicImage = ImageReader::open(Path::new(TEST_DIR_PATH).join(rom+"_dmg_blob.png").as_path()).unwrap()
-            .decode().unwrap();
 
         let raw_rgb = img.into_rgb8();
         let rgb_vec: Vec<u32> = raw_rgb.chunks(3)
@@ -56,7 +58,7 @@ mod test {
                 (r << 16) | (g << 8) | b
             })
             .collect();
-            
+
 
         loop {
             if gb.memory_read(gb.registers.pc as usize) == 0x40 {
@@ -66,9 +68,31 @@ mod test {
             gb.step();
         }
 
+        let output_png_path = rom_name.to_owned() + "_output.png";
+        save_rgb_as_png(gb.display.engine.screen.as_slice(), 160, 144, &*output_png_path).unwrap();
 
         assert_eq!(gb.display.engine.screen.len(), rgb_vec.len());
-        assert_eq!(gb.display.engine.screen, rgb_vec)
+        assert_eq!(gb.display.engine.screen, rgb_vec);
+    }
+
+    fn save_rgb_as_png(rgb_vec: &[u32], width: u32, height: u32, filename: &str) -> Result<(), Box<dyn std::error::Error>> {
+        // Step 1 & 2: Unpack u32 values and flatten to Vec<u8>
+        let mut raw_rgb: Vec<u8> = Vec::with_capacity(rgb_vec.len() * 3);
+        for &packed in rgb_vec {
+            let r = ((packed >> 16) & 0xFF) as u8;
+            let g = ((packed >> 8) & 0xFF) as u8;
+            let b = (packed & 0xFF) as u8;
+            raw_rgb.extend_from_slice(&[r, g, b]);
+        }
+
+        // Step 3: Create the image buffer
+        let img = ImageBuffer::<Rgb<u8>, _>::from_raw(width, height, raw_rgb)
+            .expect("Dimensions don't match the length of the RGB vector");
+
+        // Step 4: Save the buffer as PNG
+        img.save(Path::new(filename))?;
+        Ok(())
     }
 
 }
+
